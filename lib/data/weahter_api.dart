30bdca192edfada6a301aa9c2ef3e83655ff832d
Ali-peu <isaali032@gmail.com>
2024-06-22 17:16:22 +0500

    repository,api was done, need decode forecastModel

--- lib/data/weahter_api.dart
+++ lib/data/weahter_api.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:weather_app/domain/models/city_model.dart';
+import 'package:weather_app/domain/models/forecast_model.dart';
 
 class WeatherApi {
   final String apiKey;
@@ -17,6 +18,22 @@ class WeatherApi {
         throw Exception(response);
       }
     } on Exception catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<List<ForecastModel>?> getForecastForCity(
+      {required String cityName, int days = 3}) async {
+    try {
+      final response = await _dio.get(
+          'http://api.weatherapi.com/v1/forecast.json?key=7f885ebd7d5e42a199960954242206&q=$cityName&days=$days&aqi=no&alerts=no');
+      if (response.statusCode == 200) {
+        return []; // TODO return ForecastDays and decode them
+      }else{
+        throw Exception(response);
+      }
+    } on Exception catch (e) {
+      // TODO
       throw Exception(e);
     }
   }
--- lib/domain/repository/weather_repository.dart
+++ lib/domain/repository/weather_repository.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:weather_app/data/weahter_api.dart';
 import 'package:weather_app/domain/models/city_model.dart';
+import 'package:weather_app/domain/models/forecast_model.dart';
 
 class WeatherRepository {
   final WeatherApi weahterApi;
@@ -14,6 +15,22 @@ class WeatherRepository {
         return cityWeather;
       } else {
         throw Exception('The get weather is null, name: WeatherRepository');
+      }
+    } on Exception catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  Future<List<ForecastModel>?> getForecastForCurrentCity(
+      {required String cityName, int days = 3}) async {
+    try {
+      final forecastDays =
+          await weahterApi.getForecastForCity(cityName: cityName);
+      if (forecastDays != null) {
+        return forecastDays;
+      } else {
+        throw Exception(
+            'The get forecastDays for this city is null, name: WeatherRepository');
       }
     } on Exception catch (error) {
       throw Exception(error);
